ecb684a648500ea28ab95f14d8318e717adcf3ac
Timofey Kirillov <timofey.kirillov@flant.com>
2019-07-12 12:45:16 +0300

    Remove debug msg about using anonymous authenticator

--- pkg/v1/remote/options.go
+++ pkg/v1/remote/options.go
@@ -15,7 +15,6 @@
 package remote
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/google/go-containerregistry/pkg/authn"
@@ -51,9 +50,6 @@ func makeOptions(reg name.Registry, opts ...Option) (*options, error) {
 		auth, err := o.keychain.Resolve(reg)
 		if err != nil {
 			return nil, err
-		}
-		if auth == authn.Anonymous {
-			log.Println("No matching credentials were found, falling back on anonymous")
 		}
 		o.auth = auth
 	}
